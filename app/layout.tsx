e0b50cb4bf4f657d1af98465b96fcca4ee30fc1a
= <ssh.xero@gmail.com>
2024-11-19 20:43:24 +0100

    chore(meta): Add open graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,22 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-    title: 'Kaizen 改 善 ',
+    title: 'Kaizen 改 善',
     description:
         'Meaning continuous improvement, is a minimal Japanese-inspired task management web app for humans. ',
+    openGraph: {
+        title: 'Kaizen 改 善',
+        description:
+            'Meaning continuous improvement, is a minimal Japanese-inspired task management web app for humans.',
+        locale: 'en_US',
+        type: 'website',
+        images: {
+            url: 'https://kaizenweb.vercel.app/opengraph.png',
+            width: 1200,
+            height: 630,
+        },
+    },
+    metadataBase: new URL('https://kaizenweb.vercel.app'),
 };
 
 export default function RootLayout({
