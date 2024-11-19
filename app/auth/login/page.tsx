6abc2f405d288081ee5173e91eef860df59c7c4c
= <ssh.xero@gmail.com>
2024-11-19 12:27:02 +0100

    feat: Design base input field

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { Input } from '@/components/ui/input';
 import CenteredGridLayout from '@/layouts/CenteredGridLayout';
+import { At } from '@phosphor-icons/react';
 
 function LoginForm() {
     return (
@@ -10,6 +12,15 @@ function LoginForm() {
                 Kaizen 改 善 — Meaning continuous improvement, is a minimal
                 Japanese-inspired task management web app for humans.
             </p>
+            <div className="flex flex-col gap-2">
+                <div className="relative">
+                    <At
+                        className="absolute left-3 top-1/2 transform -translate-y-1/2 text-indigo-500"
+                        size={24}
+                    />
+                    <Input placeholder="Email address." className="pl-10 py-2 active:border-indigo-500" />
+                </div>
+            </div>
         </form>
     );
 }
