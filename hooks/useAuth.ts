683bc3bebdd7de9e371c3e6b8cf5729f1dddf752
= <ssh.xero@gmail.com>
2024-11-25 22:54:49 +0100

    feat: Implement use auth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,28 @@
+import keys from '@/config/keys';
+import { hasCookie } from 'cookies-next';
+import { useEffect, useState } from 'react';
+
+const checkSession = () => {
+    return (
+        hasCookie(keys.accessTokenKey) &&
+        localStorage.getItem(keys.userKey) != null
+    );
+};
+
+export const useAuth = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        function checkAuthStatus() {
+            setTimeout(() => {
+                setIsAuthenticated(checkSession());
+                setIsLoading(false);
+            }, 1000);
+        }
+
+        checkAuthStatus();
+    }, []);
+
+    return { isAuthenticated, isLoading };
+};
