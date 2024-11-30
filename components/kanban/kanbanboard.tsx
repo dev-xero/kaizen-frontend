1ada7d096dc7f40ef8c919453774f7c032f72475
= <ssh.xero@gmail.com>
2024-11-30 15:23:45 +0100

    feat: Implement debounced database syncing

--- components/kanban/kanbanboard.tsx
+++ components/kanban/kanbanboard.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import { DragDropContext, DropResult } from '@hello-pangea/dnd';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import DroppableColumn from './droppablecolumn';
 import Task from '@/types/task.type';
 import NewTaskModal from '../newtaskmodal';
@@ -16,15 +17,30 @@ type TasksState = {
 };
 
 export default function KanbanBoard() {
-    const { loggedInUser } = useContext(UserContext);
-    const [isLoading, setIsLoading] = useState(true);
-    const [showTaskModal, setShowTaskModal] = useState(false);
-    const [tasks, setTasks] = useState<TasksState>({
+    const defaultTaskState = {
         todo: [],
         progress: [],
         testing: [],
         completed: [],
-    });
+    };
+
+    const { loggedInUser } = useContext(UserContext);
+    const [isLoading, setIsLoading] = useState(true);
+    const [showTaskModal, setShowTaskModal] = useState(false);
+    const [tasks, setTasks] = useState<TasksState>(defaultTaskState);
+
+    const debouncedUpdateDatabase = useRef(
+        debounce(updateTasksInDatabase, 1000)
+    ).current;
+
+    // Delays function calls by some milliseconds.
+    function debounce(func: Function, delay: number) {
+        let timer: NodeJS.Timeout;
+        return (...args: unknown[]) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => func(...args), delay);
+        };
+    }
 
     // Sort and organize these tasks into their arrays.
     function organizeKanbanTasks(tasks: Task[]) {
@@ -63,12 +79,19 @@ export default function KanbanBoard() {
         setIsLoading(false);
     }
 
-    useEffect(() => {
-        if (loggedInUser?.tasks && Array.isArray(loggedInUser.tasks)) {
-            organizeKanbanTasks(loggedInUser.tasks);
-            setIsLoading(false);
+    // Delay database I/O to improve performance.
+    async function updateTasksInDatabase(updatedTasks: TasksState) {
+        try {
+            console.log('current tasks state after debounce:', updatedTasks);
+            // await fetch('/api/tasks/update', {
+            //     method: 'POST',
+            //     body: JSON.stringify(updatedTasks),
+            //     headers: { 'Content-Type': 'application/json' },
+            // });
+        } catch (error) {
+            console.error('Error updating tasks:', error);
         }
-    }, [loggedInUser]);
+    }
 
     function handleDragEnd(result: DropResult) {
         const { source, destination } = result;
@@ -76,10 +99,6 @@ export default function KanbanBoard() {
         if (!destination || source.droppableId === destination.droppableId) {
             return;
         }
-
-        console.log('Source:', source);
-        console.log('Destination:', destination);
-        console.log('Tasks State:', tasks);
 
         setTasks((prevTasks) => {
             const sourceKey = mapDroppableIdToStateKey(source.droppableId);
@@ -90,25 +109,42 @@ export default function KanbanBoard() {
             const sourceTasks = Array.from(prevTasks[sourceKey]);
             const destinationTasks = Array.from(prevTasks[destinationKey]);
 
-            // Find and remove the task from the source
+            // Find and remove the task from the source.
             const [movedTask] = sourceTasks.splice(source.index, 1);
 
+            // Singular updated task.
             const updatedTask = {
                 ...movedTask,
                 category:
                     destination.droppableId.toUpperCase() as Task['category'],
             };
 
-            // Add the task to the destination
+            // Add the task to the destination.
             destinationTasks.splice(destination.index, 0, updatedTask);
 
-            return {
+            // New tasks state as-is.
+            const newTasks = {
                 ...prevTasks,
                 [sourceKey]: sourceTasks,
                 [destinationKey]: destinationTasks,
             };
+
+            /**
+             * We will not update the database immediately since it'll trigger requests
+                for each drag/drop and sink app performance. 
+             */
+            debouncedUpdateDatabase(updateTasksInDatabase);
+
+            return newTasks;
         });
     }
+
+    useEffect(() => {
+        if (loggedInUser?.tasks && Array.isArray(loggedInUser.tasks)) {
+            organizeKanbanTasks(loggedInUser.tasks);
+            setIsLoading(false);
+        }
+    }, [loggedInUser]);
 
     // Show nothing while loading
     if (isLoading) {
